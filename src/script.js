8ea8a3e217932c7738f30ef674521489b85e6c2b
Dennis Mwangi <dennis.kamau@student.moringaschool.com>
2022-06-20 15:18:41 +0300

    testing deployment

--- src/script.js
+++ src/script.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded',()=>{
-    fetch('http://localhost:3000/players')
+    fetch('https://dennismwangi1.github.io/deploy-api/db.json')
     .then((res)=>res.json())
-    .then((players)=> players.forEach(player => renderPlayers(player))
-    )
+    .then((players)=> renderPlayers(players.players))
+    
     // .catch(alert('cannot render players'))
 
    function renderPlayers(player) {
     const heading1 = document.getElementById('heading1')
     let playerName = document.createElement('h2')
     let playerImage = document.createElement("img")
-    playerImage.src = player.image
-    playerName.textContent = player.firstName
+    playerImage.src = player[2].image
+    playerImage.style.height = "500px"
+    playerName.textContent = player[2].firstName
+    console.log(player[2].firstName)
     heading1.appendChild(playerImage)
     heading1.appendChild(playerName)
    }
